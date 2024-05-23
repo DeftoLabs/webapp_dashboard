085dbeb4edc269d771fd1531b2d088b2bd904a40
DeftoLabs <deftolabs@gmail.com>
2024-05-23 19:01:59 -0400

    user Form Impl

--- lib/ui/inputs/custom_inputs.dart
+++ lib/ui/inputs/custom_inputs.dart
@@ -36,4 +36,24 @@ class CustomInput {
       hintStyle: const TextStyle( color: Colors.grey)
     );
   }
+
+   static InputDecoration formInputDecoration ({
+    required String hint,
+    required String label,
+    required IconData icon,
+  }){
+
+    return InputDecoration(
+      border: OutlineInputBorder(
+        borderSide: BorderSide(color:const Color.fromARGB(255, 0, 0, 0).withOpacity(0.3))),
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: const Color.fromARGB(255, 0, 0, 0).withOpacity(0.3))),
+          hintText: hint,
+          labelText: label,
+          prefixIcon: Icon ( icon, color: const Color.fromARGB(255, 0, 0, 0)),
+          labelStyle: const TextStyle(color: Color.fromARGB(255, 0, 0, 0)),
+          hintStyle: const TextStyle( color: Color.fromARGB(255, 0, 0, 0)),
+
+    );
+  }
 }
--- lib/ui/views/user_view.dart
+++ lib/ui/views/user_view.dart
@@ -1,9 +1,11 @@
 
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'package:web_dashboard/models/usuaro.dart';
 import 'package:web_dashboard/providers/users_providers.dart';
 import 'package:web_dashboard/ui/cards/white_card.dart';
+import 'package:web_dashboard/ui/inputs/custom_inputs.dart';
 
 import 'package:web_dashboard/ui/labels/custom_labels.dart';
 
@@ -40,9 +42,10 @@ class _UserViewState extends State<UserView> {
       child: ListView(
         physics: const ClampingScrollPhysics(),
         children: [
-          Text('${user?.nombre}', style: CustomLabels.h1,),
+          const SizedBox(height: 20),
+          Text('${user?.nombre}', style: CustomLabels.h1, textAlign: TextAlign.center,),
 
-          const SizedBox(height: 10),
+          const SizedBox(height: 20),
 
               if( user == null) 
               WhiteCard(
@@ -52,10 +55,149 @@ class _UserViewState extends State<UserView> {
           child: const CircularProgressIndicator(),
         ),
       ),
-      Text('${ user?.nombre}')
+      _UserViewBody()
     ]
     )
     )
     ;
+  }
+}
+
+class _UserViewBody extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Table(
+        columnWidths: const {
+          0: FixedColumnWidth(250)
+        },
+
+        children: [
+          TableRow(
+            children: [
+              _AvatarContainer(),
+
+             
+              _UserViewForm()
+            ]
+          )
+        ],
+        )
+    );
+  }
+}
+
+class _UserViewForm extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return WhiteCard(
+      title: 'General Information',
+      child: Form(
+        autovalidateMode: AutovalidateMode.always,
+        child: Column(
+          children: [
+            TextFormField(
+              decoration: CustomInput.formInputDecoration(
+                hint: 'User Name', 
+                label: 'Name', 
+                icon:Icons.person),
+            ),
+            const SizedBox(height: 20,),
+            TextFormField(
+              decoration: CustomInput.formInputDecoration(
+                hint: 'User Phone', 
+                label: 'Phone', 
+                icon:Icons.phone_enabled_outlined),
+            ),
+            const SizedBox(height: 20,),
+            TextFormField(
+              decoration: CustomInput.formInputDecoration(
+                hint: 'User Email', 
+                label: 'Email', 
+                icon:Icons.email_outlined),
+            ),
+            const SizedBox(height: 20,),
+            TextFormField(
+              decoration: CustomInput.formInputDecoration(
+                hint: 'User Zone', 
+                label: 'Zone', 
+                icon:Icons.map_outlined),
+            ),
+            const SizedBox(height: 20,),
+            
+            ConstrainedBox(
+              constraints: const BoxConstraints(maxWidth: 100),
+              child: ElevatedButton(
+                onPressed: (){}, 
+                style: ButtonStyle(
+                  backgroundColor: WidgetStateProperty.all(Colors.indigo),
+                  shadowColor: WidgetStateProperty.all(Colors.transparent)
+                ),
+                child: const Row (
+                  children: [
+                    Icon(Icons.save, color: Colors.white, size: 20,),
+                    Text(' Save', style: TextStyle(color: Colors.white, fontSize: 13),)
+                  ],)
+                      ),
+            )],
+        )));
+  }
+}
+
+class _AvatarContainer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return WhiteCard(
+      width: 250,
+      child: Container(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text('Profile', style: CustomLabels.h2),
+            const SizedBox(height: 20),
+
+            SizedBox(
+              width: 150,
+              height: 160,
+              child: Stack(
+                children: [
+
+                  const ClipOval(
+                    child: Image(
+                      image: AssetImage('noimage.jpeg'))
+                      ),
+
+                  Positioned(
+                    bottom: 5,
+                    right: 5,
+                    child: Container(
+                      width: 45,
+                      height: 45,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(100),
+                        border: Border.all( color: Colors.white, width: 5)
+                    ),
+                    child: FloatingActionButton(
+                      backgroundColor: Colors.indigo,
+                      elevation: 0,
+                      child: const Icon(Icons.camera_alt_outlined, size: 20, color: Colors.white,),
+                      onPressed: () {
+                      // TODO Select image
+                    },)
+                  )
+
+              )],
+              ),
+            ),
+            const SizedBox(height: 20),
+
+            
+          ],
+        ),
+      ));
   }
 }
